6943b36d4e2c1bf1f825964b57085410d6f0d10c
Garegin <garegin.makhmuryan@evollu.com>
2022-08-07 09:22:42 +0400

    route

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,5 @@ export default {
 
 <style>
 #app {
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-
-  /*color: #2c3e50;*/
-  /*margin-top: 15px;*/
-}
-.navbar{
-  /*width: 100vh;*/
-  /*position: fixed;*/
 }
 </style>
