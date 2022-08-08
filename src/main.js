7434960bfa1e35523d7647db9e03fc26ff3f685d
Garegin <garegin.makhmuryan@evollu.com>
2022-08-08 11:51:59 +0400

    router

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ window.Echo = new Echo({
   forceTLS: true,
   encrypted: true,
   logToConsole: true,
-  wsHost: 'https://intense-forest-89445.herokuapp.com/',
-  wsPort: 6001,
+  wsHost: 'https://intense-forest-89445.herokuapp.com/:6000',
+  // wsPort: 6001,
 });
 // window.Echo = new Echo({
 //   broadcaster: 'pusher',
