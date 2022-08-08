b991c870427058a8692d29a6859c7e9332d96715
Garegin <garegin.makhmuryan@evollu.com>
2022-08-08 14:37:45 +0400

    login

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -6,7 +6,7 @@ import HomeUser from './HomeUser'
 
 export default [
   { path: '/register', name:'Register', component: Register },
-  { path: '/login', name:'Login', component: Login },
+  { path: '/', name:'Login', component: Login },
   { path: '/forgot-password', name:'ForgotPassword', component: ForgotPassword },
   { path: '/home-page/:id', name:'HomeUser', component: HomeUser },
 ]
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/Home'
 import auth from '../components/auth/auth'
-import Login from '../components/auth/Login'
 import Room from '../components/room/Room'
 
 
@@ -11,15 +9,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {path: '/',
-      name:'Login',
-      component: Login
-    },
-    // {
-    //   path: '/',
-    //   name: 'Login',
-    //   component: Login
-    // },
+
     { path: '/room/:id',
       name:'Room',
       component: Room
