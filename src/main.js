25f4272d00c17a4dedb9d30f445bd18ed94dd2b9
Garegin <garegin.makhmuryan@evollu.com>
2022-08-08 14:34:51 +0400

    make it better

--- src/main.js
+++ src/main.js
@@ -9,28 +9,28 @@ import Echo from "laravel-echo";
 
 window.Pusher = require("pusher-js");
 
-window.Echo = new Echo({
-  broadcaster: 'pusher',
-  key: "local",
-  cluster:"mt1",
-  forceTLS: true,
-  encrypted: true,
-  logToConsole: true,
-  // wsHost: window.location.hostname,
-
-  wsHost: "https://intense-forest-89445.herokuapp.com:6001" ,
-  // wsPort: 6001,
-  // wssPort: 6001,
-  enabledTransports: ['ws', 'wss']
-});
 // window.Echo = new Echo({
 //   broadcaster: 'pusher',
 //   key: "local",
 //   cluster:"mt1",
-//   forceTLS: false,
-//   wsHost: window.location.hostname,
-//   wsPort: 6001,
+//   forceTLS: true,
+//   encrypted: true,
+//   logToConsole: true,
+//   // wsHost: window.location.hostname,
+//   wsHost: "https://intense-forest-89445.herokuapp.com:6001" ,
+//   // wsPort: 6001,
+//   // wssPort: 6001,
+//   enabledTransports: ['ws', 'wss']
 // });
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: "local",
+  cluster:"mt1",
+  forceTLS: false,
+  wsHost: window.location.hostname,
+  wsPort: 6001,
+});
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
 import auth from '../components/auth/auth'
+import Login from '../components/auth/Login'
 import Room from '../components/room/Room'
 
 
@@ -10,11 +11,15 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
+    {path: '/',
+      name:'Login',
+      component: Login
     },
+    // {
+    //   path: '/',
+    //   name: 'Login',
+    //   component: Login
+    // },
     { path: '/room/:id',
       name:'Room',
       component: Room
