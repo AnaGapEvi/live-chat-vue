fc06cc8bbc9bcb23d5a7cc137b008362b1adb708
Garegin <garegin.makhmuryan@evollu.com>
2022-08-11 11:18:06 +0400

    user

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://127.0.0.1:8000/api/"'
-  // API_URL: '"https://intense-forest-89445.herokuapp.com/api/"'
+  // API_URL: '"http://127.0.0.1:8000/api/"'
+  API_URL: '"https://intense-forest-89445.herokuapp.com/api/"'
 })
